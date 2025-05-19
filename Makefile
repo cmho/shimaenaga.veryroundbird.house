fa40cac6bf69af8df37ed0a0f42407089a178235
fry69 <142489379+fry69@users.noreply.github.com>
2025-05-19 15:08:46 +0200

    feat: rework installation/deploy

---
+++ Makefile
@@ -0,0 +1,47 @@
+## Makefile for
+## - preparing files to upload to the PDS server
+## - uploading them
+## - simple server management
+##
+## Requires ssh setup to loing as root into the PDS server
+## and server name as set as SSH_HOSTNAME in .env file.
+##
+## Bugs:
+## - hardcoded paths
+
+ifeq ("$(wildcard .env)","")
+$(error "No .env file found. Please copy .env.example to .env and fill it out.")
+endif
+
+include .env
+## Exporting environment variables to sub-shells is not neccessary
+# export 
+
+all: copy-status copy-caddy install-timer
+
+copy-status: 
+	scp bin/generate-status.py $(SSH_HOSTNAME):$(dir $(STATUS_SCRIPT_PATH))
+
+copy-caddy: fmt
+	scp caddy/etc/Caddyfile $(SSH_HOSTNAME):$(PDS_DIRECTORY)/caddy/etc/caddy
+	scp caddy/webroot/* $(SSH_HOSTNAME):$(PDS_WEBROOT)
+
+fmt:
+	caddy fmt --overwrite --config caddy/etc/Caddyfile
+
+install-timer:
+	@echo "Installing timer service."
+	@(cat .env | grep STATUS ; cat install-timer.sh) | ssh $(SSH_HOSTNAME) "bash -s"
+
+generate: copy-status
+	ssh $(SSH_HOSTNAME) "$(STATUS_SCRIPT_PATH) --pds-config $(PDS_DIRECTORY)/pds.env --output $(STATUS_OUTPUT_PATH)"
+
+restart:
+	ssh $(SSH_HOSTNAME) "systemctl restart pds"
+
+status:
+	ssh $(SSH_HOSTNAME) "systemctl status pds"
+
+version:
+	curl -s https://$(PDS_HOSTNAME)/xrpc/_health
+
--- install-timer.sh
+++ install-timer.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-SERVICE_PATH="/etc/systemd/system/status-report.service"
-TIMER_PATH="/etc/systemd/system/status-report.timer"
-SCRIPT_PATH="/usr/local/bin/generate-status.py"
-OUTPUT_PATH="/pds/caddy/webroot/status.html"
-
 # Ensure script is present
-if [ ! -f "$SCRIPT_PATH" ]; then
-  echo "Error: $SCRIPT_PATH not found. Please place your script there or edit this installer."
+if [ ! -f "$STATUS_SCRIPT_PATH" ]; then
+  echo "Error: $STATUS_SCRIPT_PATH not found."
+  exit 1
+fi
+
+if [[ -z "$STATUS_SERVICE_PATH" || -z "$STATUS_TIMER_PATH" || -z "$STATUS_OUTPUT_PATH" ]]; then
+  echo "Error: One or more required variables are not set"
   exit 1
 fi
 
 # Create systemd service file
-cat <<EOF | tee "$SERVICE_PATH" > /dev/null
+cat <<EOF | tee "$STATUS_SERVICE_PATH" > /dev/null
 [Unit]
 Description=Generate server status HTML page
 After=network.target
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/env python3 $SCRIPT_PATH --pds-config /pds/pds.env --output $OUTPUT_PATH
+ExecStart=/usr/bin/env python3 $STATUS_SCRIPT_PATH --pds-config /pds/pds.env --output $STATUS_OUTPUT_PATH
 User=root
 EOF
 
 # Create systemd timer file
-cat <<EOF | tee "$TIMER_PATH" > /dev/null
+cat <<EOF | tee "$STATUS_TIMER_PATH" > /dev/null
 [Unit]
 Description=Run status-report.service every 5 minutes
 
